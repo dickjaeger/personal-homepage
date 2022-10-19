a80d20ea3ecb3d0b2005f384eda468eaf5510b26
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-19 19:27:53 +0200

    corrected issue with theme toggler

--- src/App/common/ThemeToggler/styled.js
+++ src/App/common/ThemeToggler/styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: flex-end;
+    display: inline-flex;
+    float: right;
     align-items: center;
     text-align: right;
     color: ${({ theme: { colors } }) => colors.textSecondary};
